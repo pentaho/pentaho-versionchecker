7ee33290773a4ee37d5bf3672bfa0c7f8dca2cda
Marc Batchelor <mbatchelor@pentaho.com>
2007-09-21 01:33:06 +0000

    Updates to add standard comment and fix compiler warnings.

--- src/org/pentaho/versionchecker/VersionChecker.java
+++ src/org/pentaho/versionchecker/VersionChecker.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2007 Pentaho Corporation.  All rights reserved. 
+ * This software was developed by Pentaho Corporation and is provided under the terms 
+ * of the Mozilla Public License, Version 1.1, or any later version. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho 
+ * BI Platform.  The Initial Developer is Pentaho Corporation.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+ * 
+ */
 package org.pentaho.versionchecker;
 
 import java.io.File;
@@ -15,7 +28,6 @@ import java.util.Set;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpConnectionManager;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.URI;
 import org.apache.commons.httpclient.URIException;
