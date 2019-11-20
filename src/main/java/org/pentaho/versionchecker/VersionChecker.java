27d4ff3fd535b7317b4916c2e6c4fae29c599a55
Pedro Patricio Patricio <pedro.patricio@hitachivantara.com>
2019-11-20 15:29:04 +0000

    [PPP-4283] Use of Vulnerable Components: commons-httpclient-3.0.1.jar and httpclient-4.0.1.jar (sonatype-2007-0004 | CVE-2011-1498 | CVE-2012-6153 | CVE-2012-5783)

--- src/main/java/org/pentaho/versionchecker/VersionChecker.java
+++ src/main/java/org/pentaho/versionchecker/VersionChecker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002 - 2017 Hitachi Vantara.  All rights reserved.
+ * Copyright 2002 - 2019 Hitachi Vantara.  All rights reserved.
  * 
  * This software was developed by Hitachi Vantara and is provided under the terms
  * of the Mozilla Public License, Version 1.1, or any later version. You may not use
@@ -27,7 +27,6 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.File;
